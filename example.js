bcc14ba062831e1f68470e8a08ca43dca5bdfd1c
Monalisa Octocat <octocat@github.com>
2024-04-16 17:48:55 +0600

    Creating new file

---
+++ example.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "Abir Kolin",
+  profile: "/profile.png",
+  designation: "MERN Stack Developer",
+  description:
+    "I'm a  Full-stack Developer with expertise in Node.js, Express.js, MongoDB, and front-end technologies like  React.js. I also have experience in blockchain development with Hardhat and Ether.js. Additionally, I'm familiar with tools like Next.js, Bootstrap, and Firebase Authentication. I'm eager to learn and contribute to dynamic web projects.",
+  email: "knkolin9@gmail.com",
+  phone: "+8801518603740",
+  address: "Berulia, Savar, Bangladesh",
+  github: "https://github.com/kolinabir",
+  facebook: "https://www.facebook.com/xb.anupam",
+  linkedIn: "https://www.linkedin.com/in/md-anupam-abir-kolin-68a1b6240/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+};
